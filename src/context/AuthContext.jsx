d7e63688f771e7fd7d3b2591fbcd735454268513
rachittaneja56-max <rachittaneja56@gmail.com>
2025-11-21 20:30:16 +0530

    Update AuthContext.jsx
    Refactor login handler to simplify user registration and validation.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,19 +11,16 @@ function AuthProvider({ children }) {
         if (storedUser) setUser(JSON.parse(storedUser));
         setLoading(false);
     }, []);   //runs only at start because of [] empty dependancy
-    // login handler
     const login = (email, password) => {
         const existingUser = JSON.parse(localStorage.getItem("promptvault_user"));
 
         if (!existingUser) {
-            // Auto-register on first login
             const newUser = { email, password };
             localStorage.setItem("promptvault_user", JSON.stringify(newUser));
             setUser(newUser);
             return true;
         }
 
-        // Validate existing user
         if (existingUser.email === email && existingUser.password === password) {
             setUser(existingUser);
             return true;
@@ -32,12 +29,10 @@ function AuthProvider({ children }) {
             return false;
         }
     }
-    //Logout handler
     const logout = () => {
         setUser(null);
         localStorage.removeItem("promptvault_user");
     };
-    //Provide all auth values globally 
     const value = { user, login, logout, loading };
 
     return (
